478094a2e4fac0ee8ad4470e359f61ad1b6b5b62
perezsergioace <perez.sergio.ace@gmail.com>
2020-09-06 18:01:17 -0400

    updating files

--- assignments/P1.py
+++ assignments/P1.py
@@ -14,24 +14,6 @@ pre_tax_total = item1 + item2 + item3 + item4 + item5
 total_cost = (pre_tax_total * SALES_TAX) + pre_tax_total
 print('Total cost is $', format(total_cost, '.2f'), sep='')
 
-# item_1 = 12.99
-# item_2 = 13.99
-# item_3 = 25.99
-# item_4 = float(19) # formating this integer into a floating number
-# item_5 = 69.99
-
-# new_item_1 = item_1 - (item_1 * SALE_PERCENTAGE) 
-# new_item_2 = item_2 - (item_2 * SALE_PERCENTAGE) 
-# new_item_3 = item_3 - (item_3 * SALE_PERCENTAGE) 
-# new_item_4 = item_4 - (item_4 * SALE_PERCENTAGE) 
-# new_item_5 = item_5 - (item_5 * SALE_PERCENTAGE) 
-
-# total_before_tax = new_item_1 + new_item_2 + new_item_3 + new_item_4 + new_item_5
-# print(total_before_tax)
-
-# total_cost = (total_before_tax * SALES_TAX) + total_before_tax
-# print('Total cost is', format(total_cost, '.2f'))
-
 # 2
 
 print('\"Isn\'t,\" they said.')
