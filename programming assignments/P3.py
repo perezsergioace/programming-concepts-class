36759c2bed4389b21c9628b963627fb5ed097893
perezsergioace <perez.sergio.ace@gmail.com>
2020-09-15 15:35:21 -0400

    adding screenshots for the assignment and working on the algorithm problems

---
+++ programming assignments/P3.py
@@ -0,0 +1,11 @@
+# 7a.
+
+# creating the constant variable
+EMPLOYEE_RAISE_PERCENTAGE = .025
+
+# initiating the employees current salaray
+current_salary = 0.0
+
+# initiating the raise amount for the employee
+employee_raise_amount = 0.0
+
