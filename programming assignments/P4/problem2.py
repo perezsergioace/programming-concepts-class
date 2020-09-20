e8e05f741708b9e4ca2cbe7645c0b0bb3b534b6b
perezsergioace <perez.sergio.ace@gmail.com>
2020-09-19 21:36:52 -0400

    adding problem 2 for programming assignment 4

---
+++ programming assignments/P4/problem2.py
@@ -0,0 +1,15 @@
+# In January 2020 the executive of Google purchased stock options in the company.  
+# 	The number of purchased shares were 2,365
+# 	The price of the stock was $62.50 per share
+# 	He paid stockbroker commission of 2.5% of the amount paid for the stock
+# The executive then sold the stock. 
+# 	He sold 2,000 shares of the stock
+# 	He sold the stock for $78.20 per share
+# 	He paid stockbroker commission another 3% of the amount he received for sold stock
+# Write a program that displays separate statements for each of the following: 
+# 1.	Display the amount of money paid for the stock
+# 2.	Display the amount of the commission paid to stockbroker when stock was bought
+# 3.	Display the amount the stock was sold for
+# 4.	Display the amount of commission paid to stockbroker when stocks were sold
+# 5.	Display the amount of money profited or lost from the sold stock (after stockbroker paid) Make sure your print statement clearly states if this is a profit or a loss
+
