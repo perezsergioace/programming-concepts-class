c3ba92931353ed52e23755985b57c42e76a4ef8d
perezsergioace <perez.sergio.ace@gmail.com>
2020-09-19 20:45:53 -0400

    starting to work on programming assignment 4

---
+++ programming assignments/P4/P4.py
@@ -0,0 +1,23 @@
+# Use this formula for the distance that a car travels down the interstate:  
+# 	Distance  = Speed * Time 
+# The car is traveling 82 miles per hour.  Write a program that displays the following: 
+# 1.	The distance the car will travel in 6 hours
+# 2.	The distance the car will travel in 10 hours
+# 3.	The distance the car will travel in 15 hours.  
+
+# Initiating variables
+car_speed = 82
+six_hours = 6
+ten_hours = 10
+fifteen_hours = 15
+
+# Initiating distance variables for each hours
+six_hour_distance = 0.0
+ten_hour_distance = 0.0
+fifteen_hour_distance = 0.0
+
+# Calculating the distance for 6 hours
+
+# Calculating the distance for 10 hours
+
+# Calculating the distance for 15 hours
