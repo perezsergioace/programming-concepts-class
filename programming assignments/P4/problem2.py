060471d53a13e2883b241cf22eacdfa5cd41ca72
perezsergioace <perez.sergio.ace@gmail.com>
2020-09-19 21:49:07 -0400

    starting problem 2

--- programming assignments/P4/problem2.py
+++ programming assignments/P4/problem2.py
@@ -13,3 +13,17 @@
 # 4.	Display the amount of commission paid to stockbroker when stocks were sold
 # 5.	Display the amount of money profited or lost from the sold stock (after stockbroker paid) Make sure your print statement clearly states if this is a profit or a loss
 
+# Initializing constant variables
+STOCKBROKER_COMMISION_PERCENTAGE_ONE = 0.025
+STOCKBROKER_COMMISION_PERCENTAGE_TWO = 0.03
+
+# Initializing variables
+purchased_shares = 2365
+stock_price = 62.50
+purchased_stock_amount = 0.0
+stockbroker_commision_amount_one = 0.0
+
+sold_shares = 2000
+sold_stock_price = 78.20
+sold_stock_amount = 0.0
+stockbroker_commision_amount_two = 0.0
