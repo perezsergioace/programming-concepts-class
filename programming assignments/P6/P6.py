ace033ea1dd3a259a70152e885b3c161cf9cf815
perezsergioace <perez.sergio.ace@gmail.com>
2020-09-30 21:31:32 -0400

    finished working on problem 1 in P6 file

--- programming assignments/P6/P6.py
+++ programming assignments/P6/P6.py
@@ -1,11 +1,29 @@
 # 1.) Write an if/else program that determines if a person is old enough to drink.
 # (legal age we will assume 21) 
 
+# creating variables
+age = 0.0
+
+# creating constant named variables
+legal_age = 21
+
+# prompting the user to enter their age
+age = int(input('Enter current age: '))
+
+# creatig if else statement to determine if the user's age is at the legal age to drink
+# if the age is less than 21 the program will print out a message letting the user know they are not at the legal age to drink
+if age < 21:
+    print('You are not at the legal age to drink.')
+# this else statement will print out a message letting the user know they are at the legal age since the initial if statement did not pass 
+else:
+    print('You are old enough to drink.')
+
 # 2.) Write an if/else/elif program that asks a user for their favorite color.
 # Put a statement in that if the person chooses yellow the program will return “Yellow is also my favorite color” 
 # and if the person puts red it will say “Red is the color of my favorite roses.” 
 # For all other colors the response should be “That is an interesting color choice.”
 # Test the program with yellow, red and two other colors.  
+
 
 # 3.) Write a program that asks a user for 2 numbers. 
 # If the numbers total more than 100 tell them their numbers are greater than 100, 
