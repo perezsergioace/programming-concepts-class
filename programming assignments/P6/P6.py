aa11312a1f91ef1d113b80405e2a10f3d99e7608
perezsergioace <perez.sergio.ace@gmail.com>
2020-09-30 21:01:35 -0400

    adding question prompts to the P6 python file

---
+++ programming assignments/P6/P6.py
@@ -0,0 +1,26 @@
+# 1.) Write an if/else program that determines if a person is old enough to drink.
+# (legal age we will assume 21) 
+
+# 2.) Write an if/else/elif program that asks a user for their favorite color.
+# Put a statement in that if the person chooses yellow the program will return “Yellow is also my favorite color” 
+# and if the person puts red it will say “Red is the color of my favorite roses.” 
+# For all other colors the response should be “That is an interesting color choice.”
+# Test the program with yellow, red and two other colors.  
+
+# 3.) Write a program that asks a user for 2 numbers. 
+# If the numbers total more than 100 tell them their numbers are greater than 100, 
+# if they total less than 100 tell them their numbers total less than 100.  
+
+# 4.) Draw a flowchart for the each of the decision structures in Questions 1, 2, 3
+# *you can include a snapshot or use a program to draw the flowcharts
+
+# 5.) In the book page 155. Wi-Fi Diagnostic Tree. 
+# This is an if/else program. NOTHING ELSE. 
+# Follow the diagram (page 156) and write the decision structure. 
+# You are not declaring variables, you are only writing the Decision Structure of the program.
+# You should NOT have any “answers” in your program.
+# You should only have : 
+# o	If statements
+# o	Else statements
+# o	Print statements
+# o	Response statements 
