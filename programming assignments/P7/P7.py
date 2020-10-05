180292f7c9a070f6c6a8e641e94abab4aeab0f47
perezsergioace <perez.sergio.ace@gmail.com>
2020-10-04 21:03:52 -0400

    finished working on problem 2

--- programming assignments/P7/P7.py
+++ programming assignments/P7/P7.py
@@ -11,6 +11,7 @@ month = 0.0
 # initialzing the input for the user to enter a number for the month
 month = int(input('Enter a month as a number between 1-12: '))
 
+# creating the if - elif - else statement
 if month > 9:
     print('This month is in the fourth quarter.')
 elif month <= 9 and month > 6:
@@ -27,3 +28,23 @@ else:
 # The program should then determine if the number is “Even” or “Odd”.  
 # •	You should end up with an if-elif-else, & and if- else statement
 # •	Use % 2 == 0 as the equation to figure out even or odd
+
+# creating variables
+number = 0.0
+
+# creating the prompt for the user to enter a number
+number = int(input('Please enter an integer: '))
+
+# creating the if-elif-else statement to find out if the nubmer is positive or negative or equal to zero
+if number > 0:
+    print('Positive')
+elif number < 0:
+    print('Negative') 
+else:
+    print('Zero')
+
+# creating the if - else statement to find out if the nubmer is even or odd
+if number % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
