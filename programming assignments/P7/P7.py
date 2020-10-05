08a9cea36af04da33d603821d3cd342055f958b4
perezsergioace <perez.sergio.ace@gmail.com>
2020-10-04 20:07:50 -0400

    starting to work on problem 1

---
+++ programming assignments/P7/P7.py
@@ -0,0 +1,10 @@
+# 1. You are going to figure out which months are in which quarter.
+# Write a program that asks the user for a month as a number between 1-12.
+# The program should display a message indicating whether the month is in the first quarter,
+# the second quarter, the third quarter or the fourth quarter of the year.
+# If the user inputs a number that is not between 1-12 the program should display an error.
+# You will use the if – elif – else  format.  Watch your indenting!!! 
+
+# creating variables
+month = 0.0
+
