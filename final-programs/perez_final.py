abcc630b560104b02f2a913bad3132c8b79a4f0e
perezsergioace <perez.sergio.ace@gmail.com>
2020-10-11 20:58:35 -0400

    adding final programming assignment for programming concepts class

---
+++ final-programs/perez_final.py
@@ -0,0 +1,22 @@
+# 1   Write and if - else program.  
+# creating variables
+question = 0.0
+
+question = input('Did you have fun learning Python(Enter y for yes): ')
+
+if question == 'y':
+    print('That is great to hear!')
+else:
+    print('That is too bad, but keep going you will understand it more!')
+
+
+# 2  Write and if-elif-else program.
+
+# 3  Write a program with a While Loop 
+
+# 4 Write a program with a for loop 
+
+# 5 You are going to write a program that can be used to determine your grade in this class. 
+# You will display to the user what percentage they earned in each category, 
+# as well as the total combined percentage for the class in a tabbed table format. 
+# This should be a reusable program, so do not hard code your grades into it, get input from user.  
